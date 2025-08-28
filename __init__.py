cd91b09cbb4ba940bfd0e96593a1aff69adbdda3
Victor Nascimento Bakke <gipphe@gmail.com>
2025-08-28 13:54:32 +0200

    chore: scaffold custom nodes

---
+++ __init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for metadata_statistics."""
+
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+    
+]
+
+__author__ = """Victor Nascimento Bakke"""
+__email__ = "gipphe@gmail.com"
+__version__ = "0.1.0"
+
+from .src.metadata_statistics.nodes import NODE_CLASS_MAPPINGS
+from .src.metadata_statistics.nodes import NODE_DISPLAY_NAME_MAPPINGS
+
+
